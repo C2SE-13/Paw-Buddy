ea04d4d1295d4a2e4703d55bbf517666a98d2676
Hoang Phan <hoang1311176@gmail.com>
2024-03-10 20:17:13 +0700

    [PB-4] [Dev] add fonts

--- app/src/screens/auth/SignInScreen.tsx
+++ app/src/screens/auth/SignInScreen.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {textStyles} from '../../styles/textStyles';
 
 const SignInScreen = () => {
   return (
-    <View>
-      <Text>SignInScreen</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={textStyles['bold-48']}>SignInScreen</Text>
+      <Text>abc</Text>
     </View>
   );
 };
