a0c6aef10b4f382f82c4853cf69269c8d7b3ae22
Hoang Phan <hoang1311176@gmail.com>
2024-03-14 21:02:21 +0700

    [PB-6] [Dev] add Link Component

--- app/src/assets/icons/index.ts
+++ app/src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 import AddIcon from './add.svg';
 import EyeIcon from './eye.svg';
 import EyeCloseIcon from './eye-close.svg';
+import ChevronRightIcon from './Chevron-left.svg';
 
-export {AddIcon, EyeIcon, EyeCloseIcon};
+export {AddIcon, EyeIcon, EyeCloseIcon, ChevronRightIcon};
---
+++ app/src/components/text/LinkComponent.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable react-native/no-inline-styles */
+import {StyleProp, ViewProps, TouchableOpacity} from 'react-native';
+import React, {ReactNode} from 'react';
+import {TextComponent} from '..';
+import {textStyles} from '../../styles/textStyles';
+import {colors} from '../../constants/colors';
+import {globalStyles} from '../../styles/globalStyles';
+import {ChevronRightIcon} from '../../assets/icons';
+
+interface Props {
+  onPress: () => void;
+  text: string;
+  textColor?: string;
+  textSize?: number;
+  textFont?: string;
+  textStyle?: StyleProp<ViewProps>;
+  bgColor?: string;
+  styles?: StyleProp<ViewProps>;
+  icon?: ReactNode;
+  iconLink?: boolean;
+}
+
+const LinkComponent = (props: Props) => {
+  const {
+    text,
+    onPress,
+    textColor,
+    textSize,
+    textFont,
+    textStyle,
+    bgColor,
+    styles,
+    icon,
+    iconLink,
+  } = props;
+
+  const textDefault = textStyles['regular-14'];
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        globalStyles.row,
+        globalStyles.center,
+        {
+          backgroundColor: bgColor ?? 'transparent',
+          paddingVertical: 14,
+          gap: icon ? 13 : 0,
+          borderBottomWidth: 1,
+          borderColor: colors['text-30'],
+        },
+        styles,
+      ]}>
+      {icon && icon}
+      <TextComponent
+        text={text}
+        styles={[
+          {
+            fontSize: textSize ?? textDefault.fontSize,
+            fontFamily: textFont ?? textDefault.fontFamily,
+            color: textColor ?? colors['text-100'],
+          },
+          textStyle,
+        ]}
+        flex={1}
+      />
+      {iconLink && <ChevronRightIcon />}
+    </TouchableOpacity>
+  );
+};
+
+export default LinkComponent;
