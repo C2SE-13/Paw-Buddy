834f38deee6f2a28808d0a102695872c0d7db79a
huynhbhan02 <huynhbhan71@gmail.com>
2024-04-05 21:42:19 +0700

    [PB-30][Dev] isLoading

--- app/src/navigators/AppRouters.tsx
+++ app/src/navigators/AppRouters.tsx
@@ -7,6 +7,8 @@ import {getCurrent} from '../redux/user/asyncActions';
 import withBaseComponent from '../hocs/withBaseComponent';
 import {SplashScreen} from '../screens';
 import Toast from 'react-native-toast-message';
+import {View} from 'react-native-reanimated/lib/typescript/Animated';
+import {ActivityIndicator, StyleSheet} from 'react-native';
 
 interface Props {
   dispatch: AppDispatch;
@@ -16,7 +18,7 @@ interface Props {
 const AppRouters = ({dispatch, useSelector}: Props) => {
   const {token} = useSelector(state => state.user);
   const [isShowSplash, setIsShowSplash] = useState(true);
-
+  const isLoading = useSelector((state: any) => state.app.isLoading);
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
@@ -39,6 +41,13 @@ const AppRouters = ({dispatch, useSelector}: Props) => {
 
   return (
     <>
+      {/* Nếu isLoading là true, hiển thị loading component*/}
+      {isLoading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
+
       {isShowSplash ? (
         <SplashScreen />
       ) : token ? (
@@ -50,5 +59,14 @@ const AppRouters = ({dispatch, useSelector}: Props) => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  loading: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default withBaseComponent(AppRouters);
